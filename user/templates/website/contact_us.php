23c5ede09fdaaf643d235c553e96d179512174c6
Zhe Kai <zhekai0525@gmail.com>
2022-02-15 11:24:36 +0800

    include contact_us.php

---
+++ user/templates/website/contact_us.php
@@ -0,0 +1,77 @@
+<?php include ('server.php');?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>ContactUs</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Archivo+Black&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script type = "text/javascript" src = "nav.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+		<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+    <style>
+      <?php include ('contact.css') ?>
+    </style>
+  </head>
+  <body>
+
+
+	  <div class="c-content">
+            <h3 style="padding-top:120px;font-size:70px;">Contact Us</h3>
+    </div>
+<div class="container">
+<div class="contact-back">
+  <div class="contact-box">
+    <div class="contactform">
+       <div class="email-icon" >
+         <div class="pci-contact">
+           <img src="image/emaili.png" alt="">
+         </div>
+       </div>
+       <div class="email-title">
+          <h3>Email : </h3>
+        </div>
+        <div class="email-content">
+          <h3>NextDoor2867@gmail.com</h3>
+       </div>
+       <div class="phone-icon">
+         <div class="pci-contact">
+          <img src="image/phone.png" alt="">
+         </div>
+        </div>
+        <div class="phone-title">
+          <h3>Phone Number : </h3>
+        </div>
+        <div class="phone-content">
+          <p>017-9818884</p>
+       </div>
+       <div class="address-icon">
+         <div class="pci-contact">
+            <img src="image/location.png" alt="">
+         </div>
+        </div>
+        <div class="address-title">
+          <h3>Address : </h3>
+        </div>
+        <div class="address-content">
+          <h3>No 58, Jalan Wong Ah Jang, 25100 Kuantan Pahang</h3>
+       </div>
+    </div>
+
+    <div class="mid-line"></div>
+    <div class="location-text">
+        <h3 style="padding-top:120px;">Our location on map</h3>
+    </div>
+     <div class="map">
+       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d16306211.454912845!2d94.35616424999998!3d3.808208299999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31c8bb4eb5aac8d3%3A0xd7c65a6facf18bb9!2sNext%20Door!5e0!3m2!1szh-CN!2smy!4v1608013744180!5m2!1szh-CN!2smy" width="100%" height="100%" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+    </div>
+  </div>
+</div>
+</div>
+
+
+
+</body>
+</html>
